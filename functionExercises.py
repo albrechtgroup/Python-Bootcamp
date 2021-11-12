00bb40fb091b0aa96d2f7c1385e1259d43d9fca9
Andy Albrecht <aalbrecht33733@gmail.com>
2021-11-12 16:01:58 -0500

    Python function exercises: first half

---
+++ functionExercises.py
@@ -0,0 +1,105 @@
+######################
+# Function Exercises:
+
+""" Write a function called 'product' that accepts two
+    parameters and returns the product of the two 
+    parameters(multiply them together) """
+from math import prod
+
+def product(a, b):
+    return a*b
+print(product(2, 2))
+
+""" Write a function called 'return_day'. This function
+    takes in one parameter(a num from 1-7) and returns
+    the day of the week(1 is Sunday, 2 is Monday, etc.) 
+    If the number is less than 1 or greater than 7, 
+    the function should return 'None' """
+# Hint: Store the days of the week in a list
+
+
+""" Write a function called 'last_element'. This 
+    function takes in one parameter(a list) and returns
+    last value in the list. It should return None if
+    empty. """
+
+
+""" Write a function called 'number_compare'. This 
+    function takes in two parameters(both numbers). 
+    If the first is greater than the second, this
+    function returns "First is greater". If the 2nd
+    number is greater than first, the function returns
+    "Second is greater" Otherwis the function returns
+    "Numbers are equal" """
+
+
+""" Write a function called 'single_letter_count". 
+    This func takes in two parameters(2 strings)
+    The first param should be a word and the 2nd
+    a letter. The func returns the num of times
+    that letter appears in the word. The function 
+    should be case-sensitive(input doesnt matter)
+    If the letter is not found in the word, the 
+    function returns zero """
+# Hint: take advantage of count() method.
+
+
+""" Write a function called 'multiple_letter_count'
+    This function takes in one parameter(a string)
+    and returns a dictionary with the keys being
+    the letters and values being the count of letter
+    """
+# Hint: Use a dictionary comprehension and count()
+
+
+""" Write a function called 'list_manipulation'. This
+    function should taek in four parameters(a list,
+    command, location and value) """
+
+
+
+""" Write a function called 'is_palindrome. A Palindrome
+    is a word, phrase, number, or other sequence of
+    characters which reads teh same backward or
+    forward. This function should take in one parameter
+    and returns True or False depending on whether
+    it is a Palindrome. """
+
+
+""" Writer a function called 'frequency'. This function
+    accepts a list and a search_term(this will always
+    be a primitive value) and returns the number of
+    times the search_term appears in the list. """
+
+
+""" Write a function called 'multiply_even_numbers'. This
+    function accepts a list of numbers and returns
+    the product of all even numbers in the list. """
+
+
+""" Write a function called 'capitalize'.This function
+    accepts a string and returns the same string with
+    the first letter capitalized. You may want to use
+    slices to help you out. """
+
+
+""" Write a function called 'compact'. This function 
+    accepts a list and  returns a list of values that
+    are 'truthy' values, wihtout any of the falsey
+    values. """
+
+
+""" Write a function called 'intersection'. This 
+    function should accept two lists and return a
+    list with the values that are in both input 
+    lists. """
+
+
+""" Write a function called 'partition'. This func
+    accepts a list and a callback funct(which you 
+    can assume returns True or False). The function 
+    should iterater over each element in the list and
+    invoke the callback func at each iteration. """
+
+
+
