41c88a823c6f57f0b644d1191957c826e920e6a7
Andy Albrecht <aalbrecht33733@gmail.com>
2021-11-12 16:30:02 -0500

    Python function exersices: Second half

--- functionExercises.py
+++ functionExercises.py
@@ -183,5 +183,23 @@ def intersection(l1, l2):
     should iterater over each element in the list and
     invoke the callback func at each iteration. """
 
+""" "Traditional" Version:
+Here's my solution that doesn't use a list comprehension:
+
+I have two lists, to hold the true and false values
+I loop through the list, calling fn with each value in the list
+If the result is True, I append the value to the trues  list
+Otherwise, I append the value to the falses  list
+At the end I return a list that contains both the trues and falses  lists """
+
+def partition(lst, fn):
+    trues = []
+    falses = []
+    for val in lst:
+        if fn(val):
+            trues.append(val)
+        else:
+            falses.append(val)
+    return [trues, falses]
 
 
