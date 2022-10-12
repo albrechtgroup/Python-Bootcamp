6e4fec68dfe12e003869fe3cbbc185a89bd56c5d
Andy Albrecht <aalbrecht33733@gmail.com>
2022-10-12 16:21:41 -0400

    Lambdas

---
+++ functsBuiltIn.py
@@ -0,0 +1,13 @@
+###############
+# Lambdas:
+# Short, One line of code Only
+# Old stuff..Review
+def square(num):
+    return num * num
+print(square(7))
+
+# lambda stored in a variabl. Not usually..
+# Automatically returned, dont need a return statement
+square2 = lambda num: num * num
+print(square2(9))
+
